c703df3519f261cd8bdf1ba97f89009adf4ea2d3
kevin Dongmo Ndonfack <dongmo300@gmail.com>
2026-03-14 09:06:35 -0400

    main implementation

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -4,18 +4,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
 
 public class AnalyticsCounter {
 	
 	private ISymptomWriter ecriture_symptoms;
 	private ISymptomReader lecture_symptoms ;
-	
-	private static int headacheCount = 0;
-	private static int rashCount = 0;	
-	private static int pupilCount = 0;	
 	
 	public AnalyticsCounter(ISymptomReader reader, ISymptomWriter writer) {
 		this.ecriture_symptoms = writer;
@@ -63,35 +56,5 @@ public class AnalyticsCounter {
 		
 	}
 	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
-
-		int i = 0;
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-
-			line = reader.readLine();	// get another symptom
-		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
-	}
+	
 }
---
+++ Project02Eclipse/src/com/hemebiotech/analytics/Main.java
@@ -0,0 +1,23 @@
+package com.hemebiotech.analytics;
+
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ISymptomReader lecteurFichier = new ReadSymptomDataFromFile("Project02Eclipse/symptoms.txt");
+		ISymptomWriter ecriveurFichier = new WriteSymptomDataToFile();
+		AnalyticsCounter analyseurFichier = new AnalyticsCounter(lecteurFichier, ecriveurFichier);
+		
+		List<String> listeSymptoms = analyseurFichier.getSymptoms();
+		
+		Map<String, Integer> quantiteSymptoms = analyseurFichier.countSymptoms(listeSymptoms);
+		
+		Map<String, Integer> trieSymptoms = analyseurFichier.sortSymptoms(quantiteSymptoms);
+		
+		analyseurFichier.writeSymptoms(trieSymptoms);
+
+	}
+
+}
